46abd0a4fc7103bb2b8665c4a2f22f889cdec27c
johnder1972 <john.derkinderen@yahoo.com>
2025-12-31 10:19:54 +0100

    mois en fr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -7,7 +7,9 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
     provideRouter(routes),
     provideHttpClient() // Utile pour les appels API futurs
   ]
 };
+
