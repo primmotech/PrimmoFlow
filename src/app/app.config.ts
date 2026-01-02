a9656088fa34dad18b06f10b1b625a140f636bb6
johnder1972 <john.derkinderen@yahoo.com>
2026-01-02 20:25:33 +0100

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
-import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     { provide: LOCALE_ID, useValue: 'fr-FR' },
     provideRouter(routes),
-    provideHttpClient() // Utile pour les appels API futurs
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }) // Utile pour les appels API futurs
   ]
 };
 
